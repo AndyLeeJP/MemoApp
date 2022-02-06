b2fe4bc6b7cdd901aa6efeaa699ffafa7e40a23c
vaperLee <rize1126jesse@gmail.com>
2022-02-05 19:12:52 -0500

    Install @expo/vector-icons

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -17,7 +17,7 @@ export default function MemoDetailScreen(){
                     Wake up and do my home work
                 </Text>
             </ScrollView>
-            <CircleButton style={{top: 160, bottom: 'auto'}}>+</CircleButton>
+            <CircleButton style={{top: 160, bottom: 'auto'}} name="edit-2" />
         </View>
     );
 }
