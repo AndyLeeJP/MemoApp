9a119144957fa409eb604ac034944aa731cd4a22
vaperLee <rize1126jesse@gmail.com>
2022-03-24 01:48:23 -0400

    Remove console.log

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 import firebase from "firebase";
 
 import MemoList from '../components/MemoList';
@@ -29,7 +29,6 @@ export default function MemoListScreen(props){
       unsubscribe = ref.onSnapshot((snapshot) =>{
         const userMemos = [];
         snapshot.forEach((doc) =>{
-          console.log(doc.id, doc.data());
           const data = doc.data();
           userMemos.push({
             id: doc.id,
@@ -39,8 +38,7 @@ export default function MemoListScreen(props){
         });
         setMemos(userMemos);
         setLoading(false);
-      }, (error) => {
-        console.log(error);
+      }, () => {
         setLoading(false);
         Alert.alert('Failed to load data');
       });
